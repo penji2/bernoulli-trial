b6ac3bda262b6cbd5516a0c16b609180fd6b0cd5
penji2 <krystian.pegowski@gmail.com>
2019-05-12 14:21:21 +0200

    Initial commit, added scratch file

---
+++ venv/bernoulli-trial.py
@@ -0,0 +1,30 @@
+class Bernoulli:
+
+    def __init__(self):
+        pass
+
+    def trial(self):  # Bernoulli trial function, probability of success = p, loss = 1 - p
+        pass
+
+    def geo_generator(self):  # random variable X generator with geometric distribution
+        pass
+
+    def histogram(self):  # function that generates histogram of distribution of random X variable
+        pass
+
+
+class Testing:
+
+    def __init__(self):
+        pass
+
+    def first_model(self):
+        pass
+
+    def second_model(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
+
