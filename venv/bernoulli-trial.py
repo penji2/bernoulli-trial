14a77592ba076a8b0bfdd64ecfa38c3dc3233607
penji2 <krystian.pegowski@gmail.com>
2019-05-12 16:43:01 +0200

    Added trial function, scratch for generator

--- venv/bernoulli-trial.py
+++ venv/bernoulli-trial.py
@@ -1,13 +1,34 @@
+import random
+import sys
+
+
 class Bernoulli:
 
-    def __init__(self):
-        pass
+    def __init__(self, probability_parameter):
+        if 0 <= probability_parameter <= 1:
+            self.p = probability_parameter
+        else:
+            sys.exit("p must be between 0 and 1")
 
     def trial(self):  # Bernoulli trial function, probability of success = p, loss = 1 - p
-        pass
+        tmp = random.random()
+        if tmp <= self.p:
+            result = 1
+        else:
+            result = 0
+        return result  # returns 0 - loss, 1 - success
 
     def geo_generator(self):  # random variable X generator with geometric distribution
-        pass
+        table_of_success, k_array = list()
+
+        # for i in range(20):
+        #     count = 1
+        #     for j in range(1000):
+        #         tmp = self.trial()
+        #         count += 1
+        #         table_of_success.append(tmp)
+        #         if tmp == 1:
+        #             k_array.append(count)
 
     def histogram(self):  # function that generates histogram of distribution of random X variable
         pass
@@ -26,5 +47,5 @@ class Testing:
 
 
 if __name__ == '__main__':
-    pass
-
+    obj = Bernoulli(0.5)
+    obj.geo_generator()
